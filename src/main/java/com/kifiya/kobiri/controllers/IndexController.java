2e79cbebbc1d36285f357f32e69eda89fee08200
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-01-11 16:29:02 +0100

    connexion deconnexion front

--- src/main/java/com/kifiya/kobiri/controllers/IndexController.java
+++ src/main/java/com/kifiya/kobiri/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.kifiya.kobiri.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,14 +9,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(){
+    @RequestMapping(value = {"/", "index", "acceuil"}, method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("connexion", "connexion");
         return "index";
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String connexion(){
-        return "connecter";
+    public String indexConnected(Model model){
+        model.addAttribute("connexion", "deconnexion");
+        return "index";
     }
 
 }
