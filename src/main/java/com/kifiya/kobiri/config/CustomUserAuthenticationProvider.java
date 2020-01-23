97f9a3e29c0df8691fc2c91216a737e8040bd9a0
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-01-23 16:56:10 +0100

    connexion redirect

--- src/main/java/com/kifiya/kobiri/config/CustomUserAuthenticationProvider.java
+++ src/main/java/com/kifiya/kobiri/config/CustomUserAuthenticationProvider.java
@@ -1,18 +1,31 @@
 package com.kifiya.kobiri.config;
 
+import com.kifiya.kobiri.models.user.User;
 import com.kifiya.kobiri.repositories.UserRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
-//@Component
-public class CustomUserAuthenticationProvider /*implements AuthenticationProvider*/ {
+import java.util.Collections;
 
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomUserAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
     private UserRepository userRepository;
 
-    //@Autowired
-    public CustomUserAuthenticationProvider(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
-
-    /*@Override
+    @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String userMail = authentication.getName();
         String password = authentication.getCredentials().toString();
@@ -26,5 +39,5 @@ public class CustomUserAuthenticationProvider /*implements AuthenticationProvide
     @Override
     public boolean supports(Class<?> aClass) {
         return aClass.equals(UsernamePasswordAuthenticationToken.class);
-    }*/
+    }
 }
--- src/main/java/com/kifiya/kobiri/config/WebSecurityConfig.java
+++ src/main/java/com/kifiya/kobiri/config/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.kifiya.kobiri.config;
 
-//@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
-    /*private CustomUserAuthenticationProvider customUserAuthenticationProvider;
+@Configuration
+@EnableWebSecurity
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public WebSecurityConfig(CustomUserAuthenticationProvider customUserAuthenticationProvider){
-        this.customUserAuthenticationProvider = customUserAuthenticationProvider;
-    }
+    private CustomUserAuthenticationProvider customUserAuthenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +34,11 @@ public class WebSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(customUserAuthenticationProvider);
-    }*/
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+    }
 
 }
