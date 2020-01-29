29179090dbe0138776326f3b9b67ae4c9fe98aa8
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-01-29 14:25:36 +0100

    registration ok

--- src/main/java/com/kifiya/kobiri/services/UserService.java
+++ src/main/java/com/kifiya/kobiri/services/UserService.java
@@ -5,6 +5,7 @@ import com.kifiya.kobiri.models.user.User;
 import com.kifiya.kobiri.repositories.RoleRepository;
 import com.kifiya.kobiri.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -17,14 +18,14 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
-    //private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User findUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
     public User save(User user) {
-        //user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         Role userRole = roleRepository.findByRole("ADMIN");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         return userRepository.save(user);
