942db16828fdd425f9179db295b614e4b39470db
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-05-21 13:15:27 +0200

    #36 Gestion erreur

--- src/main/java/com/kifiya/kobiri/controllers/GestionErreur.java
+++ src/main/java/com/kifiya/kobiri/controllers/GestionErreur.java
@@ -4,11 +4,13 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/")
 public class GestionErreur implements ErrorController /**, AccessDeniedHandler */{
     @Override
     public String getErrorPath() {
@@ -22,13 +24,13 @@ public class GestionErreur implements ErrorController /**, AccessDeniedHandler *
             Integer statusCode = Integer.valueOf(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error/404";
+                return "erreur/404";
             }
             else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error/500";
+                return "errreur/500";
             }
         }
-        return "error";
+        return "erreur/erreur";
     }
 
 
