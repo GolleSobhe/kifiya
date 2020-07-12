686e0cb31f90d49e6b6cc67953c318dccb6765bf
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-07-12 12:49:22 +0200

    #18 init stepper transfert

--- src/main/resources/static/js/kobiri.js
+++ src/main/resources/static/js/kobiri.js
@@ -1,1 +1,54 @@
-var i = 0;
+$(document).ready(function () {
+  var navListItems = $('div.setup-panel div a'),
+          allWells = $('.setup-content'),
+          allNextBtn = $('.nextBtn'),
+          allBackBtn = $('.backBtn');
+
+  allWells.hide();
+
+  navListItems.click(function (e) {
+      e.preventDefault();
+      var $target = $($(this).attr('href')),
+              $item = $(this);
+
+      if (!$item.hasClass('disabled')) {
+          navListItems.removeClass('btn-primary').addClass('btn-default');
+          $item.addClass('btn-primary');
+          allWells.hide();
+          $target.show();
+          $target.find('input:eq(0)').focus();
+      }
+  });
+
+  allNextBtn.click(function(){
+      var curStep = $(this).closest(".setup-content"),
+          curStepBtn = curStep.attr("id"),
+          nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+          curInputs = curStep.find("input[type='text'],input[type='url']"),
+          isValid = true;
+
+      $(".form-group").removeClass("has-error");
+      for(var i=0; i<curInputs.length; i++){
+          if (!curInputs[i].validity.valid){
+              isValid = false;
+              $(curInputs[i]).closest(".form-group").addClass("has-error");
+          }
+      }
+
+      if (isValid)
+          nextStepWizard.removeAttr('disabled').trigger('click');
+  });
+
+  allBackBtn.click(function() {
+    var curStep = $(this).closest(".setup-content");
+    var curStepBtn = curStep.attr("id");
+
+    var currStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().children("a");
+    var prevStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+
+    prevStepWizard.trigger('click');
+    currStepWizard.attr("disabled", "disabled");
+  });
+
+  $('div.setup-panel div a.btn-primary').trigger('click');
+});
