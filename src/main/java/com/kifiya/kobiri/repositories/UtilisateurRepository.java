7cdc72a805298d90eca2ac1eb04d1d59d7a476e0
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-09-13 00:50:44 +0200

    #25 desable security

--- src/main/java/com/kifiya/kobiri/repositories/UtilisateurRepository.java
+++ src/main/java/com/kifiya/kobiri/repositories/UtilisateurRepository.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
