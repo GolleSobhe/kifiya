dae0bc2c16e097036c695ca934245d4e22d4b1c5
fiyaHolloSobhe <bahpetit0686@gmail.com>
2020-10-09 06:01:42 +0200

    #63 correction bug liste boutique ok

--- src/main/java/com/kifiya/kobiri/repositories/IndexRepository.java
+++ src/main/java/com/kifiya/kobiri/repositories/IndexRepository.java
@@ -2,6 +2,7 @@ package com.kifiya.kobiri.repositories;
 
 import com.kifiya.kobiri.models.Boutique;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
@@ -12,24 +13,23 @@ import java.util.Set;
 
 @Repository
 public class IndexRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     String SELECT_MAP = "SELECT taux, frais, nom, ville,description  FROM PARAMETRE, BOUTIQUE ;";
 
-    public IndexRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public IndexRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     public Map<String, Object> obtenirPrametre() {
         Map<String,Object> results = new HashMap<>();
         Set<Boutique> boutiques = new HashSet<>();
-        jdbcTemplate.query(SELECT_MAP, (ResultSet resultSet) -> {
-            while (resultSet.next()) {
+
+        namedParameterJdbcTemplate.query(SELECT_MAP, (ResultSet resultSet) -> {
                 boutiques.add(Boutique.builder().nom(resultSet.getString("nom"))
                         .ville(resultSet.getString("ville"))
                         .description(resultSet.getString("description")).build());
                 results.put("taux", resultSet.getDouble("taux"));
                 results.put("frais", resultSet.getDouble("frais"));
-            }
         });
         results.put("boutiques", boutiques);
         return results;
